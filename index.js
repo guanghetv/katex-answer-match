aaae38e15516c0a0efed476bdde6ae9414e46f18
YshadowZ <809077530@qq.com>
2017-08-29 20:03:09 +0800

    fix: 异常处理

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 +(function() {
     'use strict';
     var katex_answer_match = function(origin, dest){
+        origin = origin || ''
+        dest = dest || ''
         return origin.replace(/\s+/g, '') === dest.replace(/\s+/g, '')
     }
     this.katex_answer_match = katex_answer_match
