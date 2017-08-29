864133e66f06234ba9efb4d8eb732f77610f969e
YshadowZ <809077530@qq.com>
2017-08-29 18:43:26 +0800

    export function via this

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function katex_answer_match(origin, dest) {
-    return origin.replace(/\s+/g, '') === dest.replace(/\s+/g, '')
-}
++(function() {
+    'use strict';
+    var katex_answer_match = function(origin, dest){
+        return origin.replace(/\s+/g, '') === dest.replace(/\s+/g, '')
+    }
+    this.katex_answer_match = katex_answer_match
+}.call(this))
