f4ad31b6f1d9f2083f38381f34495073c09c97b5
YshadowZ <809077530@qq.com>
2017-08-29 20:14:00 +0800

    fix: 异常处理

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 +(function() {
     'use strict';
     var katex_answer_match = function(origin, dest){
-        origin = origin || ''
-        dest = dest || ''
+        if (origin === undefined || origin === null) {
+            origin = ''
+        }
+        if (dest === undefined || dest === null) {
+            dest = ''
+        }
+        if (typeof origin !== 'string' || typeof dest !== 'string') {
+            throw Error('params need string')
+        }
         return origin.replace(/\s+/g, '') === dest.replace(/\s+/g, '')
     }
     this.katex_answer_match = katex_answer_match
